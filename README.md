ace582fd5ba23dfd602b423a0cbdea9e594530e3
Adrian Suzdorf <suzdorfa@gmail.com>
2023-02-24 16:47:48 +0100

    Database set

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Wordle clone
 
 Just for fun and practise React.
-`
+
+## Database
+
+In the project I use `json-server` package.
+More info: https://www.npmjs.com/package/json-server
+
+### How to install
+
+`npm install -g json-server`
+
+### How to use
+
+`json-server ./data/db.json --port 3001`
+JSON file should be available here: http://localhost:3001/pokemon
--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const DATABASE_URL = "http://localhost:3001/pokemon";
+
+  const [pokemonName, setPokemonName] = useState();
+
+  useEffect(() => {
+    return () => {
+      //  Fetch a list of 5 letters pokemon ( 49 pokemon in the house)
+      fetch(DATABASE_URL)
+        .then((res) => res.json())
+        .then((json) => {
+          const randomNumber = Math.random() * json.length;
+          const normalizeRandomNumber = Math.floor(randomNumber);
+          const randomPokemonName = json[normalizeRandomNumber];
+          setPokemonName(randomPokemonName.name);
+        });
+    };
+  }, [setPokemonName]);
+
   return (
     <div className="App">
-      <h1>Wordle Clone</h1>
+      <h1>Wordlemon (Wordle clone)</h1>
     </div>
   );
 }
